97c65778c61922022a92dce9235f7cc9c97f8289
Sander van Wickeren <sandervanwickeren@gmail.com>
2018-01-16 10:54:42 +0100

    Creations of Permissions class to handle permission problems

---
+++ app/src/main/java/com/example/sander/pictureswipe/GrandPermissions.java
@@ -0,0 +1,14 @@
+package com.example.sander.pictureswipe;
+
+/**
+ * Class that handles all permission requests, and the
+ * corresponding response.
+ */
+
+public class GrandPermissions {
+
+    public void askReadPermission() {
+    }
+
+    // Response. https://developer.android.com/training/permissions/requesting.html#java
+}
--- app/src/main/java/com/example/sander/pictureswipe/MainActivity.java
+++ app/src/main/java/com/example/sander/pictureswipe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sander.pictureswipe;
 
+import android.Manifest;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -7,6 +8,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -32,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Set backstack listener
         getSupportFragmentManager().addOnBackStackChangedListener(new backstackListener());
-
 
     }
 
