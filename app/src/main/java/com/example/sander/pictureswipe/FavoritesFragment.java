9406a61782502bbec72bb20f58796e381fa0ace6
SandervWickeren <sandervanwickeren@gmail.com>
2018-01-23 20:14:03 +0100

    Click to view bigger image also implemented in favorites

--- app/src/main/java/com/example/sander/pictureswipe/FavoritesFragment.java
+++ app/src/main/java/com/example/sander/pictureswipe/FavoritesFragment.java
@@ -5,9 +5,11 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ListAdapter;
@@ -35,12 +37,9 @@ public class FavoritesFragment extends Fragment {
 
         SqliteDatabase db = SqliteDatabaseSingleton.getInstance(getActivity().getApplicationContext());
 
-        /*ListView listView = view.findViewById(R.id.favoriteList);
-        String[] items = new String[] {"item1", "item2", "item3"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, items);
-        listView.setAdapter(adapter);*/
-
         GridView gridView = view.findViewById(R.id.favImageGrid);
+        gridView.setOnItemClickListener(new RepeatGridListener());
+
         PictureGridAdapter pictureGridAdapter = new PictureGridAdapter(getContext(), db.selectAllBin("favorites"));
         gridView.setAdapter(pictureGridAdapter);
 
@@ -57,5 +56,32 @@ public class FavoritesFragment extends Fragment {
             }
         }
         cursor.close();
+    }
+
+    private class RepeatGridListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+            // Get the cursor from the adapter
+            Cursor cursor = ((PictureGridAdapter)adapterView.getAdapter()).getCursor();
+
+            // Move it to the correct position
+            cursor.moveToPosition(position);
+
+            // Retrieve the path
+            String path = cursor.getString(cursor.getColumnIndex("path"));
+
+            // Launch new fragment using the path
+            Bundle bundle = new Bundle();
+            bundle.putString("path", path);
+            FullscreenImageFragment fragment = new FullscreenImageFragment();
+            fragment.setArguments(bundle);
+            //((MainActivity)getActivity()).replaceFragment(fragment);
+            FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+            fragment.show(ft, "dialog");
+
+
+        }
     }
 }
